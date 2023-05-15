dc6bbb43a739e581bd5742b0a5932e82753c9bfc
izzat1998 <izzattilla706@gmail.com>
2023-05-15 16:54:34 +0500

    container get info by name deleted

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -5,7 +5,7 @@ from tgbot.keyboards.reply import start_keyboard
 
 
 async def admin_start(message: Message):
-    await message.reply("Hello, admin!", reply_markup=start_keyboard)
+    await message.reply("Hello, admin !!!", reply_markup=start_keyboard)
 
 
 def register_admin(dp: Dispatcher):
