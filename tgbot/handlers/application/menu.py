da6f9b089608460ef75bc2424107ea51f379bc91
izzat1998 <izzattilla706@gmail.com>
2023-05-15 11:51:44 +0500

    add document

--- tgbot/handlers/application/menu.py
+++ tgbot/handlers/application/menu.py
@@ -5,7 +5,7 @@ from tgbot.keyboards.reply import application_keyboard
 
 
 async def get_application(message: types.Message):
-    await message.answer(text="Выберите заявку", reply_markup=application_keyboard)
+    await message.answer(text="Выберите заявку !!", reply_markup=application_keyboard)
 
 
 def register_menu(dp: Dispatcher):
